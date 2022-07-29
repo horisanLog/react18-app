3482ad116d3510a2ad94380b5bc488f5e75b6015
horisanLog <ttie7374@gmail.com>
2022-07-30 08:09:09 +0900

    Transition

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,13 @@
 import React, { memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import { TopPage } from "./pages";
+import Transition from "./pages/transition";
 
 export const Routing: React.FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<TopPage />} />
+      <Route path="/transition" element={<Transition />} />
     </Routes>
   );
 });
