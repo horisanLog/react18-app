db64faaa03403859e5590ba6308a16e7c41f9988
horisanLog <ttie7374@gmail.com>
2022-07-30 12:16:33 +0900

    再帰型

--- src/pages/recursive.tsx
+++ src/pages/recursive.tsx
@@ -65,6 +65,49 @@ const test: JSONValueTypeObject = {
 };
 // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑お試し型推論の勉強
 
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓再帰型の型推論の勉強
+// 参考資料
+// https://github.com/microsoft/TypeScript/pull/33050
+// https://www.typescriptlang.org/play?#code/PTAEGEAsHtoZwJYDsDmoBGBTALgd05kqAK6KqjYCeADpqAG5yjLaYBOAZgIYDGdCTLumjFsFSJgCwAKBCge0JHGxsuLJsnF0OmLtmJtMTDtDahdPSADpQAdQTZIoAMxWA7ABpQiutA4y5RzoFJRU1JW8iKloMUVAkaDFNFnZuPlBcLiZDAFtoekwAEysZALAATRF5LiIOZELvOLzDUCERMUcBZiJMAA8uHOoAG0wALmijAFpGSZTOXiNS2TAASTFSIopoDDpHPVBKKoViIYaEsUMdM2xtoIoaOkPiMtaWwsw6pGQ0ZER3rXuMQccEwQw4NgAKpAuplBKAhggcg4XrhoZZQIUEIUkAByMR9ARJJRYug1bqseZ8Lw1BrHU47DKmADWRReXDYIiQDVwDicXHhDmwI0ZbCZJWkLwAYqZzP1BiMvJ0NExzqAdFkEOhhZpXAA2UYyCagABqXCGxEwAHk2ABBNiqSgAHghAD5QABeUAQ0AAH1AdodjtN5qttvtXCdrpdAG4lnIbUREcNMDlCNg9AhFIyTg1IFwCvDYCyGgiWeIBF50JRQEjet8XncjTzHK1E0gKWlMOKjcGLdaAxGAEzOt2e71+geUS0cXuhycj2PSOad-3hqczs19sOB12y1hcpjz2f9tfDqOgADeoAAvnGwFCumahtBcMYZfyFINDBJiQX3p8HEzIg-FAAApABlS0ADkPBRNEnFwZkmCrUBLnYNhvi2ZhsBBMFxUNB4wLgLNPWUDDyD9JBiByLAzD9YRoBGMlKJOIZfSIxQAG0AF12KvTiWUoUZQDI75uOE0DiKIa9FxkEJlFlAZk3A9N9DgCDoIkqSPUvGRQFaeg1CGIQRmElQLVg6R9I2NgkAGMZQAAIlA3QkEmZ9iGoRzLP0jloByYSLz0-T4ns4THIACQQFBIB4QxlG84L9LkcAalVLhCgaDhiCQHhsCAjR21uCQOKiB4ktAORPMKPQHIACgASg9N0L1vKyb0smS7y9CRDBxJhml2bYmNstUORyAFXDcHZ01Q0FdBBeJEiMVsGmBUAAAUACUDWWF5IGwbBqDgUYQHeegtWgFA4CsJE4vgPxsCsT9gAmOA4oQahsGANLOR4b5Jjej6vsmZxJjcYB9sO47TuAFBeWIdBnv84A7o5YiOG+iEHnA4HvuoVjgGcZwAAYAFYSaAA
+type ValueOrArray<T> = T | ArrayOfValueOrArray<T>;
+interface ArrayOfValueOrArray<T> extends Array<ValueOrArray<T>> {}
+
+type Json = string | number | boolean | null | Json[] | { [key: string]: Json };
+
+const exampleStatusJSON: Json = {
+  id: "1",
+  team_name: "事業本部",
+  children: [
+    {
+      id: "2",
+      team_name: "東日本",
+      children: [
+        {
+          id: "4",
+          team_name: "広報部",
+          children: [],
+        },
+      ],
+    },
+    {
+      id: "3",
+      team_name: "西日本",
+      children: [
+        {
+          id: "5",
+          team_name: "営業部",
+          children: [],
+        },
+        {
+          id: "6",
+          team_name: "開発部",
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑再帰型の型推論の勉強
 
 const data: Team = {
   id: "1",
