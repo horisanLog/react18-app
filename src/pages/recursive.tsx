c922fd2bc11b0a6562350fdb1de1fc6483e253e6
horisanLog <ttie7374@gmail.com>
2022-07-30 11:02:21 +0900

    オリジナル

--- src/pages/recursive.tsx
+++ src/pages/recursive.tsx
@@ -1,21 +1,28 @@
 import React, {
-  useEffect,
-  useState,
-  useLayoutEffect,
   memo,
-  Suspense,
 } from "react";
-import useSWR from "swr";
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import TreeItem from "@mui/lab/TreeItem";
+
+import { styled, Box } from "@mui/material";
+
+const CustomBox = styled(Box)(() => ({
+  width: "100%",
+  borderColor: "#22BA9D",
+  borderRadius: "0.5rem",
+  boxShadow: "1px 1px 2px 2px rgba(0, 0, 0, 0.6)",
+  letterSpacing: "0.7px",
+  color: "#454545",
+}));
 
 export type Team = {
   id: string;
   team_name: string;
   team_name_kana: string;
   team_leader_name: string;
+  depth: number;
   children: Team[];
 };
 
@@ -24,18 +31,21 @@ const data: Team = {
   team_name: "事業本部",
   team_name_kana: "ジギョウホンブ",
   team_leader_name: "ジョンソン",
+  depth: 0,
   children: [
     {
       id: "2",
-      team_name: "人事部",
+      team_name: "東日本",
       team_name_kana: "ヒガシニホン",
       team_leader_name: "マイケル",
+      depth: 1,
       children: [
         {
           id: "4",
           team_name: "広報部",
-          team_name_kana: "ヒガシニホン",
-          team_leader_name: "マイケル",
+          team_name_kana: "コウホウ",
+          team_leader_name: "ジャクソン",
+          depth: 2,
           children: [],
         },
       ],
@@ -45,19 +55,22 @@ const data: Team = {
       team_name: "西日本",
       team_name_kana: "ニシシニホン",
       team_leader_name: "ビヨンセ",
+      depth: 1,
       children: [
         {
           id: "5",
           team_name: "営業部",
-          team_name_kana: "ヒガシニホン",
-          team_leader_name: "マイケル",
+          team_name_kana: "エイギョウ",
+          team_leader_name: "ビートル",
+          depth: 2,
           children: [],
         },
         {
           id: "6",
           team_name: "開発部",
-          team_name_kana: "ヒガシニホン",
-          team_leader_name: "マイケル",
+          team_name_kana: "カイハツ",
+          team_leader_name: "ビル",
+          depth: 2,
           children: [],
         },
       ],
@@ -65,16 +78,32 @@ const data: Team = {
   ],
 };
 
+interface Props {
+  teams: Team;
+}
+
 export const RecursivePage: React.FC = memo(() => {
-  const renderTree = (nodes: Team) => (
-    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.team_name}>
-      {Array.isArray(nodes.children)
-        ? nodes.children.map((node) => renderTree(node))
+
+  const teamTree = (teams: Team) => (
+    <TreeItem key={teams.id} nodeId={teams.id} label={teams.team_name}>
+      {Array.isArray(teams.children)
+        ? teams.children.map((team) => teamTree(team))
         : null}
     </TreeItem>
   );
 
-  console.log(renderTree(data));
+  const TeamTree: React.FC<Props> = ({ teams }: Props) => {
+    return (
+      <>
+        <Box sx={{ pl: `${teams.depth}0px` }}>
+          {teams.team_name}
+          {teams.children?.map((team) => {
+            return <TeamTree key={team.team_name} teams={team} />;
+          })}
+        </Box>
+      </>
+    );
+  };
 
   return (
     <>
@@ -86,8 +115,21 @@ export const RecursivePage: React.FC = memo(() => {
         defaultExpandIcon={<ChevronRightIcon />}
         sx={{ height: "100%", flexGrow: 1, width: "30%", overflowY: "auto" }}
       >
-        {renderTree(data)}
+        {teamTree(data)}
       </TreeView>
+
+      {/* オリジナルで実装した場合 */}
+      <Box
+        sx={{
+          mt: 5,
+          height: "100%",
+          width: "30%",
+          overflowY: "auto",
+          background: "white"
+        }}
+      >
+        <TeamTree teams={data} />
+      </Box>
     </>
   );
 });
