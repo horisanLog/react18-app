7e4d7aa0cd791938900b55ca7c7e5e907a19d176
horisanLog <ttie7374@gmail.com>
2022-08-14 09:06:25 +0900

    lazy

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,15 +1,52 @@
-import React, { memo } from "react";
+import React, { memo, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import { TopPage } from "./pages";
-import { RecursivePage } from "./pages/recursive";
-import Transition from "./pages/transition";
+const TopPage = lazy(() =>
+  import("./pages").then(({ TopPage }) => ({
+    default: TopPage,
+  }))
+);
+const RecursivePage = lazy(() =>
+  import("./pages/recursive").then(({ RecursivePage }) => ({
+    default: RecursivePage,
+  }))
+);
+const Transition = lazy(() => import("./pages/transition"));
 
 export const Routing: React.FC = memo(() => {
+  window.addEventListener("load", function () {
+    function print_PerformanceEntries() {
+      // getEntries() を使用してすべてのパフォーマンスエントリのリストを取得します。
+      var p = performance.getEntries();
+      for (var i = 0; i < p.length; i++) {
+        console.log("PerformanceEntry[" + i + "]");
+        print_PerformanceEntry(p[i]);
+      }
+    }
+    function print_PerformanceEntry(perfEntry: any) {
+      var properties = ["name", "entryType", "startTime", "duration"];
+
+      for (var i = 0; i < properties.length; i++) {
+        // それぞれのプロパティをチェックします。
+        var supported = properties[i] in perfEntry;
+        if (supported) {
+          var value = perfEntry[properties[i]];
+          console.log("... " + properties[i] + " = " + value);
+        } else {
+          console.log("... " + properties[i] + " is NOT supported");
+        }
+      }
+    }
+    console.log("test");
+    print_PerformanceEntries();
+  });
+
   return (
-    <Routes>
-      <Route path="/" element={<TopPage />} />
-      <Route path="/transition" element={<Transition />} />
-      <Route path="/recursive" element={<RecursivePage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/transition" element={<Transition />} />
+        <Route path="/recursive" element={<RecursivePage />} />
+      </Routes>
+    </Suspense>
   );
 });
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,13 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <Routing />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
